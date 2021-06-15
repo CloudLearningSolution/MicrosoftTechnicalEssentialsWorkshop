e6c6337c584fb85e6d8c4b755f95e2ff47db9d9e
DavidS <david3651@live.com>
2021-06-15 15:37:57 -0400

    Initial commit

---
+++ DroneFactory/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Drone Factory*****\n");
+            // Retrieve connection string provider
+            string dataProvider =
+                ConfigurationManager.AppSettings["provider"];
+            string connectionString =
+                ConfigurationManager.AppSettings["connectionString"];
+
+            // Retrieve data provider
+            DbProviderFactory factory = DbProviderFactories.GetFactory(dataProvider);
+
+            // Subsequently retrieve connection object
+            using (DbConnection connection = factory.CreateConnection())
+            {
+                if (connection == null)
+                {
+                    ShowError("Connection");
+                    return;
+                }
+                Console.WriteLine($"Your connection object is a: {connection.GetType().Name}");
+                connection.ConnectionString = connectionString;
+                connection.Open();
+
+                //Create command object
+                DbCommand command = factory.CreateCommand();
+                if (command == null)
+                {
+                    ShowError("Command");
+                    return;
+                }
+                Console.WriteLine($"Your command object is a: {command.GetType().Name}");
+                command.Connection = connection;
+                command.CommandText = "Select * From Inventory";
+
+                //Print data
+                using (DbDataReader dataReader = command.ExecuteReader())
+                {
+                    Console.WriteLine($"Your data reader object is a: {dataReader.GetType().Name}");
+                    Console.WriteLine("\n***** Drone Inventory *****");
+                    while (dataReader.Read())
+                        Console.WriteLine($"-> Drone #{dataReader["DroneId"]} is a {dataReader["Make"]}.");
+                }
+            }
+            Console.ReadLine();
+        }
+        private static void ShowError(string objectName)
+        {
+            Console.WriteLine($"There was an issue creating the {objectName}");
+            Console.ReadLine();
+        }
+    }
+}
