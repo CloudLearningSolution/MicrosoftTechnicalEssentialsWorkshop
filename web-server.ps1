fc3eb7b0856f4cf5020a2b9d7786833f51acc71b
DavidS <david3651@live.com>
2021-07-22 15:26:15 -0400

    updated web-server script

--- web-server.ps1
+++ web-server.ps1
@@ -23,4 +23,4 @@
 # SOFTWARE.
 
 Add-WindowsFeature Web-Server
-Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Hello World from host $($env:computername) !"
+Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Pharmakinematics host $($env:computername) !"
