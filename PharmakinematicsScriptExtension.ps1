e274441a2965163b01bd20e9ccc8aaead3f5d0f8
DavidS <david3651@live.com>
2021-07-25 00:03:45 -0400

    update

--- PharmakinematicsScriptExtension.ps1
+++ PharmakinematicsScriptExtension.ps1
@@ -88,7 +88,7 @@ Start-Process -FilePath $filePath -ArgumentList $vsOptions
 
 Start-Process -FilePath "vs_Community" -WorkingDirectory "C:\Users\globaladministrator\Pharmakinematics\repo" -ArgumentList "--passive", "--norestart", "--add Microsoft.VisualStudio.Workload.Azure", "--add Microsoft.VisualStudio.Workload.Data", "--add Microsoft.VisualStudio.Workload.ManagedDesktop", "--add Microsoft.VisualStudio.Workload.NetCoreTools", "--add Microsoft.VisualStudio.Workload.NetWeb", "--add Component.GitHub.VisualStudio", "--add Microsoft.Component.ClickOnce", "--add Microsoft.Net.Component.4.7.2.SDK", "--add Microsoft.VisualStudio.Component.Git", "--includeOptional"
 
-Start-Process -FilePath "shutdown.exe" -ArgumentList "/r","/t 900"
+Start-Process -FilePath "shutdown.exe" -ArgumentList "/r","/t 1200"
 
 # TODO : DO NOT EDIT THE COMMENTED SECTION BELOW. COMMITED BY DAVID SANTANA.
 #Region Begin.. Implementing Fault Tolerant Virtual Machine Scaled Sets
