7721edda9323f324b689a7645eb1bb0769102631
DavidS <david3651@live.com>
2021-07-25 15:22:34 -0400

    updated

--- PharmakinematicsScriptExtension.ps1
+++ PharmakinematicsScriptExtension.ps1
@@ -84,8 +84,8 @@ $vsOptions = @(
     #$optionsWait
 )
 # Start Multiple Processes but "WAIT" for Last Process to Complete
-Start-Process -FilePath $filePath -ArgumentList $vsOptions -Wait
-Start-Process -FilePath "vs_Community" -WorkingDirectory "C:\Users\globaladministrator\Pharmakinematics\repo" -ArgumentList "--passive", "--norestart", "--add Microsoft.VisualStudio.Workload.Azure", "--add Microsoft.VisualStudio.Workload.Data", "--add Microsoft.VisualStudio.Workload.ManagedDesktop", "--add Microsoft.VisualStudio.Workload.NetCoreTools", "--add Microsoft.VisualStudio.Workload.NetWeb", "--add Component.GitHub.VisualStudio", "--add Microsoft.Component.ClickOnce", "--add Microsoft.Net.Component.4.7.2.SDK", "--add Microsoft.VisualStudio.Component.Git", "--includeOptional" -Wait
+Start-Process -FilePath $filePath -ArgumentList $vsOptions
+Start-Process -FilePath "vs_Community" -WorkingDirectory "C:\Users\globaladministrator\Pharmakinematics\repo" -ArgumentList "--passive", "--norestart", "--add Microsoft.VisualStudio.Workload.Azure", "--add Microsoft.VisualStudio.Workload.Data", "--add Microsoft.VisualStudio.Workload.ManagedDesktop", "--add Microsoft.VisualStudio.Workload.NetCoreTools", "--add Microsoft.VisualStudio.Workload.NetWeb", "--add Component.GitHub.VisualStudio", "--add Microsoft.Component.ClickOnce", "--add Microsoft.Net.Component.4.7.2.SDK", "--add Microsoft.VisualStudio.Component.Git", "--includeOptional"
 # TODO : David, try this command on Sunday
 Start-Process -FilePath "shutdown.exe" -ArgumentList "/r","/t 1200" -Wait
 
