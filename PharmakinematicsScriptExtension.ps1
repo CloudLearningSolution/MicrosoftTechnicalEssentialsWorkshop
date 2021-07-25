0ec8c384bd58df0fdcf77228eb0d1f154504be9c
DavidS <david3651@live.com>
2021-07-25 18:09:20 -0400

    update

--- PharmakinematicsScriptExtension.ps1
+++ PharmakinematicsScriptExtension.ps1
@@ -85,9 +85,11 @@ $vsOptions = @(
 )
 # Start Multiple Processes but "WAIT" for Last Process to Complete
 Start-Process -FilePath $filePath -ArgumentList $vsOptions
+
 Start-Process -FilePath "vs_Community" -WorkingDirectory "C:\Users\globaladministrator\Pharmakinematics\repo" -ArgumentList "--passive", "--norestart", "--add Microsoft.VisualStudio.Workload.Azure", "--add Microsoft.VisualStudio.Workload.Data", "--add Microsoft.VisualStudio.Workload.ManagedDesktop", "--add Microsoft.VisualStudio.Workload.NetCoreTools", "--add Microsoft.VisualStudio.Workload.NetWeb", "--add Component.GitHub.VisualStudio", "--add Microsoft.Component.ClickOnce", "--add Microsoft.Net.Component.4.7.2.SDK", "--add Microsoft.VisualStudio.Component.Git", "--includeOptional"
+
 # TODO : David, try this command on Sunday
-Start-Process -FilePath "shutdown.exe" -ArgumentList "/r","/t 1200" -Wait
+# Start-Process -FilePath "shutdown.exe" -ArgumentList "/r","/t 1200" -Wait
 
 # TODO : DO NOT EDIT THE COMMENTED SECTION BELOW. COMMITED BY DAVID SANTANA.
 #Region Begin.. Implementing Fault Tolerant Virtual Machine Scaled Sets
