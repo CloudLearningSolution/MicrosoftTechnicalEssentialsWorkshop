dc8e44cc26e2f0878991181e3c2c5cf306f9c6cb
DavidS <david3651@live.com>
2021-07-30 15:51:47 -0400

    Added InstallAzPS Script

---
+++ InstallAzPS.ps1
@@ -0,0 +1,16 @@
+﻿#Install Microsoft Azure PowerShell and required dependencies#
+#Region Begin
+<#
+#$webClient = New-Object -TypeName System.Net.WebClient
+#$webClient.Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+
+#[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
+#Register-PSRepository -Default -Verbose
+#Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+
+#Install-Module -Name PowerShellGet -Force
+
+#Install-Module -Name Az -Repository PSGallery -Force
+#>
+#Region End
