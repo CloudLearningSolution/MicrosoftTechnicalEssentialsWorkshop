15835fed3a2b81e2875104e41b636aec3829b146
DavidS <david3651@live.com>
2021-07-30 16:51:27 -0400

    updated Azure CLI install command

--- InstallAzPS.ps1
+++ InstallAzPS.ps1
@@ -12,5 +12,7 @@
 #Install-Module -Name PowerShellGet -Force
 
 #Install-Module -Name Az -Repository PSGallery -Force
+
+# Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
 #>
 #Region End
