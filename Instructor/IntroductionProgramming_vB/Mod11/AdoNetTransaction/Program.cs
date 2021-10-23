860a03f887813cfad23d42f280f1f34b2ecc40c7
DavidS <david3651@live.com>
2021-10-23 18:11:11 -0400

    Updated ADO Code

--- Instructor/IntroductionProgramming_vB/Mod11/AdoNetTransaction/Program.cs
+++ Instructor/IntroductionProgramming_vB/Mod11/AdoNetTransaction/Program.cs
@@ -4,8 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using AutoLotDAL;
-//using AutoLotDAL.ConnectedLayer;
+using AutoLotDAL;
+using AutoLotDAL.ConnectedLayer;
 using static System.Console;
 
 namespace AdoNetTransaction
@@ -15,9 +15,25 @@ namespace AdoNetTransaction
 	static void Main(string[] args)
 	{
 		WriteLine("***** Simple Transaction Example *****\n");
+			// Exception Handling.
+			bool throwEx = true;
 
+			Write("Do you want to throw an exception (Y or N): ");
+			var userAnswer = ReadLine();
+			if (userAnswer?.ToLower() == "n")
+			{
+				throwEx = false;
+			}
+			// Please update 
+			var dal = new InventoryDAL();
+			dal.OpenConnection(@"Data Source=(local)\<Place holder for YOUR current systems SQL Database Server Name>;Integrated Security=SSPI;" +
+				"Initial Catalog=AutoLot");
 
-		ReadLine();
+			// Transaction.
+			dal.ProcessCreditRisk(throwEx, 5);
+			WriteLine("Check CreditRisk table for results");
+
+			ReadLine();
 	}
 }
 }
--- Instructor/IntroductionProgramming_vB/Mod11/AutoLotDAL/Models/NewCar.cs
+++ Instructor/IntroductionProgramming_vB/Mod11/AutoLotDAL/Models/NewCar.cs
@@ -1,11 +1,12 @@
-﻿/* //TODO :Delete this line and wait for further instruction.
-using System.Net.NetworkInformation;
+﻿using System.Net.NetworkInformation;
 
 namespace AutoLotDAL.Models
 {
 	public class NewCar
 	{
-
+		public int CarId { get; set; }
+		public string Color { get; set; }
+		public string Make { get; set; }
+		public string PetName { get; set; }
 	}
 }
-*/ //TODO :Delete this line and wait for further instruction.
